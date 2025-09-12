e2a7601631ca81569a8422eb4cc85bffd2c36054
Kelsi Anderson <kelsi.anderson@okta.com>
2025-09-12 12:14:56 -0700

    feat: adding envs

--- main/sentry.js
+++ main/sentry.js
@@ -1,14 +1,38 @@
 // Dynamically load Sentry browser SDK
+
 (function () {
+  // Define your config for each environment
+  const SENTRY_CONFIGS = {
+    tus: {
+      dsn: "https://d433d747a8af0820757f35be62ee08be@o27592.ingest.us.sentry.io/4509985515241472",
+      release: "docs-v2-tus",
+      environment: "tus",
+    },
+    sus: {
+      dsn: "https://d433d747a8af0820757f35be62ee08be@o27592.ingest.us.sentry.io/4509985515241472",
+      release: "docs-v2-sus",
+      environment: "sus",
+    },
+    prod: {
+      dsn: "https://d433d747a8af0820757f35be62ee08be@o27592.ingest.us.sentry.io/4509985515241472",
+      release: "docs-v2",
+      environment: "prod",
+    },
+  };
+
+  const ENV = window.ENV || "prod"; 
+  const config = SENTRY_CONFIGS[ENV];
+
   var script = document.createElement('script');
   script.src = 'https://js.sentry-cdn.com/66b846e0c465bc5aa1b02a23463be6f5.min.js';
   script.crossOrigin = 'anonymous';
   script.async = true;
   script.onload = function () {
     window.Sentry.init({
-      dsn: "https://d433d747a8af0820757f35be62ee08be@o27592.ingest.us.sentry.io/4509985515241472",
+      dsn: config.dsn,
       sendDefaultPii: true,
-      release: "docs-v2",
+      release: config.release,
+      environment: config.environment,
       integrations: [
         // Add integrations here if needed
       ],
