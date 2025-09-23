ab21050e4c47180eb707bcb131d08691002ae0c0
Kelsi Anderson <kelsi.anderson@okta.com>
2025-09-23 12:34:54 -0700

    removing commented out work

--- main/analyticsloader.js
+++ main/analyticsloader.js
@@ -32,40 +32,6 @@ export function AnalyticsConsentLoader() {
     return () => window.removeEventListener('message', handleConsentChange);
   });
 }
-  // useEffect to parse initial consent and set up OneTrust callback
-//   useEffect(() => {
-//     function updateConsentsFromCookie() {
-//       const optanonConsent = getCookie("OptanonConsent");
-//       const groups = optanonConsent
-//         ?.replace("OptanonConsent=", "")
-//         .split("&")
-//         .find((s) => s.includes("groups="));
-//       if (groups) {
-//         const parsedConsents = decodeURIComponent(groups.replace("groups=", ""))
-//           .split(",")
-//           .map((s) => {
-//             const [group, value] = s.split(":");
-//             return value === "1" ? group : null;
-//           })
-//           .filter(Boolean);
-//         setConsents(parsedConsents);
-//       }
-//     }
-
-//     // Initial load
-//     updateConsentsFromCookie();
-
-//     // OneTrust callback for consent changes
-//     window.handleOptanonConsent = function () {
-//       if (window.OnetrustActiveGroups) {
-//         const parsedConsents = window.OnetrustActiveGroups.split(",").slice(1, -1);
-//         setConsents(parsedConsents);
-//       }
-//     };
-//   }, []);
-
-//   return null; // This component does not render anything
-// }
 
 //Why doesn't OneTrust need "consent-required"?
 
