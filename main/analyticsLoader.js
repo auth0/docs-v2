9dd37a34679512ab73fb19732db79c51519d42e7
Kelsi Anderson <kelsi.anderson@okta.com>
2025-09-30 09:03:12 -0700

    progressing with heap work

--- main/analyticsLoader.js
+++ main/analyticsLoader.js
@@ -7,62 +7,83 @@ const State = {
   Timeout: "timeout",
 };
 
-
+const HEAP_CONFIGS = {
+  sus: {
+    ID: "",
+    release: "docs-v2-sus",
+    environment: "sus",
+  },
+  prod: {
+    ID: "",
+    release: "docs-v2",
+    environment: "prod",
+  },
+    };
+    //windows in binding here
+const AUTH0_DOCS_ENV = window.AUTH0_DOCS_ENV || "prod"; 
+const config = HEAP_CONFIGS[AUTH0_DOCS_ENV];
 
 const ANALYTICS_EVENTS = "ANALYTICS_EVENTS";
-// Give 15s for scripts to load after consent is given
 const TIMEOUT_DELAY = 15000;
 let timeoutStarted = false;
 
-// Helper to search for specific cookie by name
+//currently working
 const getCookies = (name) => {
-    console.log("Searching for cookie:", name);
     const cookieParts = document.cookie.split("; ");
-  //return matching cookie if found
-  //might be expected
-  //if the coookie doesn't exist, it doens't load the scirpts because there aren't any cookies yet
-  //so if the optanonconsent cookie doesn't exist, it won't load any scripts
-  console.log("Current cookies:", document.cookie);
   return cookieParts.find((s) => s.includes(`${name}=`));
 };
 
+
+function heap(heapId) {
+    console.log("Loading Heap with ID:", heapId);
+  // load one-trust-script
+  const script = document.createElement("script");
+  script.innerHTML = `window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],
+  heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},
+  window.heap.clientConfig.shouldFetchServerConfig=!1;
+  var a=document.createElement("script");
+  a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";
+  var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);
+  var n=["init",
+  "startTracking",
+  "stopTracking",
+  "track","resetIdentity",
+  "identify","identifyHashed",
+  "getSessionId","getUserId","getIdentity",
+  "addUserProperties","addEventProperties",
+  "removeEventProperty","clearEventProperties",
+  "addAccountProperties",
+  "addAdapter",
+  "addTransformer",
+  "addTransformerFn",
+  "onReady",
+  "addPageviewProperties",
+  "removePageviewProperty",
+  "clearPageviewProperties",
+  "trackPageview"],
+  i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);
+  window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};
+  for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+  heap.load(${heapId});`
+  script.id = "heap-script";
+  script.type = "text/plain";
+  console.log("Adding consent-required class to Heap script");
+  script.classList.add('consent-required:C0002');  
+  script.async = true;
+  document.body.append(script);
+  console.log("Heap script added to the document");
+}
 
 // Consent check and Heap script loader
 const optanonConsent = getCookies("OptanonConsent");
 let hasConsent = false;
 let hasDeniedConsent = false;
 
-if (optanonConsent) {
-    console.log("OptanonConsent cookie found on preload:", optanonConsent);
-  const groups = optanonConsent
-    .replace("OptanonConsent=", "")
-    .split("&")
-    .find((s) => s.includes("groups="));
-  if (groups) {
-    console.log("Consent groups found on preload:", groups);
-    const consents = decodeURIComponent(groups.replace("groups=", ""))
-      .split(",")
-      .map((s) => {
-        const [group, value] = s.split(":");
-        return value === "1" ? group : value === "0" ? "denied" : null;
-      })
-      .filter(Boolean);
-    hasConsent = consents.some((c) => c !== "denied");
-    hasDeniedConsent = consents.every((c) => c === "denied");
-    console.log("Parsed consents on preload:", consents);
-    console.log("Has consent:", hasConsent, "Has denied consent:", hasDeniedConsent);
-  } else {
-    console.log("No consent groups found in OptanonConsent cookie on preload");
-  }
-}
-
 const loadConsentScripts = (consents) => {
   // Necessary consents will always be present, no need to load any scripts.
   // Otherwise none
   console.log("Current consents:", consents);
   if (consents.length <= 1) {
-    // If consent was given manually start timeout to prevent dead ends
-    //returns early
     console.log("No consents given, checking for manual consent");
     if (window.OneTrust?.IsAlertBoxClosedAndValid() || !!getCookies("OptanonAlertBoxClosed")) {
       startTimeout();
@@ -73,7 +94,6 @@ const loadConsentScripts = (consents) => {
   // Only query scripts with type "text/plain" to avoid reinjecting the same script twice
   const scripts = document.querySelectorAll('script[class^="consent-required"][type="text/plain"]');
   if (scripts.length === 0) {
-    console.log("No consent-required scripts found");
     return;
   }
   const consentsGiven = consents.map((c) => "C000" + c);
@@ -92,6 +112,7 @@ const loadConsentScripts = (consents) => {
     console.log(`Loading script ${s.id} with consents:`, consentsGiven);
     document.head.appendChild(s);
   });
+  heap("2269341915");
   // Start timeout for scripts loading
   startTimeout();
 };
@@ -116,11 +137,12 @@ const optanonWrapper = () => {
     if (window.OnetrustActiveGroups) {
       const consents = window.OnetrustActiveGroups.split(",").slice(1, -1);
       loadConsentScripts(consents);
-      console.log("OnetrustActiveGroups found, loading consent scripts:", consents);
+      heap("2269341915");
     }
   } catch (e) {
     console.warn("Error loading analytics scripts from wrapper", e);
   }
+
 };
 
 /**
@@ -129,35 +151,35 @@ const optanonWrapper = () => {
  */
 const analyticsLoader = () => {
   try {
-    // Initialize scripts state
-    window.analyticsScripts = {
-      heap: State.NotLoaded,
-      optimizely: State.NotLoaded,
-      pendo: State.NotLoaded,
-      qualtrics: State.NotLoaded,
-      usabilla: State.NotLoaded,
-      ...window.analyticsScripts,
-    };
-
-    // Try to get consents from the OptanonConsent cookie
-    const groups = optanonConsent
-      ?.replace("OptanonConsent=", "")
-      .split("&")
-      .find((s) => s.includes("groups="));
-    console.log("OptanonConsent cookie found:", groups);
-    if (groups) {
-      const consents = decodeURIComponent(groups.replace("groups=", ""))
-        .split(",")
-        .map((s) => {
-          const [group, value] = s.split(":");
-          return value === "1" ? group : null;
-        })
-        .filter(Boolean);
-      loadConsentScripts(consents);
-      console.log("Consents found in OptanonConsent cookie, loading consent scripts:", consents);
+ if (optanonConsent) {
+    console.log("OptanonConsent cookie found on preload:", optanonConsent);
+  const groups = optanonConsent
+    .replace("OptanonConsent=", "")
+    .split("&")
+    .find((s) => s.includes("groups="));
+  if (groups) {
+    console.log("Consent groups found on preload:", groups);
+    const consents = decodeURIComponent(groups.replace("groups=", ""))
+      .split(",")
+      .map((s) => {
+        const [group, value] = s.split(":");
+        return value === "1" ? group : value === "0" ? "denied" : null;
+      })
+      .filter(Boolean);
+    hasConsent = consents.some((c) => c !== "denied");
+    hasDeniedConsent = consents.every((c) => c === "denied");
+    console.log("Parsed consents on preload:", consents);
+    console.log("Has consent:", hasConsent, "Has denied consent:", hasDeniedConsent);
+    if (hasConsent) {
+      heap("2269341915");
+      console.log("Heap loaded on preload because consent is present");
     }
-
-    // Set OneTrust callback to check again after consent changes
+  }
+   else {
+    console.log("No consent groups found in OptanonConsent cookie on preload");
+  }
+}
+   // Set OneTrust callback to check again after consent changes
     window.OptanonWrapper = optanonWrapper;
   } catch (e) {
     console.warn("Error loading analytics scripts from cookie", e);
--- main/heap.js
+++ main/heap.js
@@ -1,26 +1,20 @@
 //use innerhtml for this and just copy the script tags into this
-function heap(heapId) {
-    console.log("Loading Heap with ID:", heapId);
-  // load one-trust-script
-  const script = document.createElement("script");
-  script.innerHTML = `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};
-    var r=document.createElement("script"); r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js",
-    r.onload=function(){window.analyticsScripts = { ...window.analyticsScripts, heap: 'loaded'};window.dispatchEvent(new CustomEvent('ANALYTICS_SCRIPT_STATE_CHANGE', { detail: { id: 'heap', state: 'loaded' } }));},
-    r.onerror=function(){window.analyticsScripts = { ...window.analyticsScripts, heap: 'failed'};window.dispatchEvent(new CustomEvent('ANALYTICS_SCRIPT_STATE_CHANGE', { detail: { id: 'heap', state: 'failed' } }));};
-    var a=document.getElementsByTagName("script")[0]; a.parentNode.insertBefore(r,a);
-    for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
-    heap.load(${heapId});`
-  script.id = "heap-script";
-  script.type = "text/plain";
-  console.log("Adding consent-required class to Heap script");
-  script.classList.add('consent-required:C0002');  
-  script.async = true;
-  document.body.append(script);
-  console.log("Heap script added to the document");
-}
-document.addEventListener("readystatechange", () => {
-    heap("2269341915");
-    console.log("Heap function executed on readystatechange");
-});
-
-//try logging more
+// function heap(heapId) {
+//     console.log("Loading Heap with ID:", heapId);
+//   // load one-trust-script
+//   const script = document.createElement("script");
+//   script.innerHTML = `window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],
+//   heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","identifyHashed","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+//   heap.load(${heapId});`
+//   script.id = "heap-script";
+//   script.type = "text/plain";
+//   console.log("Adding consent-required class to Heap script");
+//   script.classList.add('consent-required:C0002');  
+//   script.async = true;
+//   document.body.append(script);
+//   console.log("Heap script added to the document");
+// }
+// document.addEventListener("readystatechange", () => {
+//     heap("2269341915");
+//     console.log("Heap function executed on readystatechange");
+// });
