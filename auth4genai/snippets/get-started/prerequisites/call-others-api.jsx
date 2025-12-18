1ca60910367ab9ff44693bfd990e7a005d4c9810
Lucy Zhou <141781699+lrzhou25@users.noreply.github.com>
2025-12-17 16:07:29 -0800

    A4AA QS Feedback (#413)
    * Added API identifier language
    
    * Added dashboard links
    
    * Updated dashboard links in qs
    
    * Added language about custom API
    
    * Added dashboard links to main content
    
    * Removed Auth0 APIs link
    
    * Update call-others-api.jsx

--- auth4genai/snippets/get-started/prerequisites/call-others-api.jsx
+++ auth4genai/snippets/get-started/prerequisites/call-others-api.jsx
@@ -51,28 +51,21 @@ export const Prerequisites = ({
 
   if (createAuth0ApiStep) {
     steps.push(
-      <Step key="auth0-api" title="Create an Auth0 API">
+      <Step key="auth0-api" title="Create a Custom API">
+        A Custom API is an API you own that you want to secure using Auth0.
         <ul>
           <li>
             In your Auth0 Dashboard, go to{" "}
-            <strong>Applications &gt; APIs</strong>.
+            <a href="https://manage.auth0.com/#/apis" target="_blank"><strong>Applications &gt; APIs</strong></a>.
           </li>
-          <li>Create a new API with an identifier (audience).</li>
+          <li>Create a new API with an identifier (audience), for example, <code>https://my-custom-api.com</code>.</li>
           <li>
             Once API is created, go to the APIs{" "}
             <strong>Settings &gt; Access Settings</strong> and enable{" "}
             <strong>Allow Offline Access</strong>.
           </li>
-          <li>Note down the API identifier for your environment variables.</li>
+          <li>Note down the API identifier for your environment variables. You will use the API identifier as the audience parameter in token requests (<code>AUTH0_AUDIENCE</code>).</li>
         </ul>
-        To learn more about Auth0 APIs, read{" "}
-        <a
-          href="https://auth0.com/docs/get-started/auth0-overview/set-up-apis"
-          target="_blank"
-        >
-          APIs
-        </a>
-        .
       </Step>
     );
   }
@@ -94,7 +87,7 @@ export const Prerequisites = ({
         <br />
         <ul>
               <li>
-               From the Settings page of the API that you just created, click the <strong>Add Application</strong> button in the right top corner. This will open a modal to create a new Custom API Client.
+               From the <strong>Settings</strong> page of the API that you just created, click the <strong>Add Application</strong> button in the right top corner. This will open a modal to create a new Custom API Client.
               </li>
               <li>
                 Give your Custom API Client a name in the Application Name field and click the <strong>Add</strong> button to create a new Custom API Client.
@@ -113,7 +106,7 @@ export const Prerequisites = ({
       The Connected Accounts flow uses the <a href="https://auth0.com/docs/manage-users/my-account-api" target="_blank">My Account API</a> to create and manage connected accounts for a user across supported external providers.<br/><br/>
       In the Auth0 Dashboard, configure the My Account API:
       <ul>
-        <li>Navigate to <strong>Authentication &gt; APIs</strong>, locate the My Account API banner, and select <strong>Activate</strong> to activate the Auth0 My Account API.</li>
+        <li>Navigate to <a href="https://manage.auth0.com/#/apis" target="_blank"><strong>Applications &gt; APIs</strong></a>, locate the My Account API banner, and select <strong>Activate</strong> to activate the Auth0 My Account API.</li>
         <li>Once activated, select <strong>Auth0 My Account API</strong> and then select the <strong>Applications</strong> tab.
           <ul>
             <li>Toggle your client application to authorize it to access the My Account API.</li>
@@ -130,7 +123,7 @@ export const Prerequisites = ({
       After your web application has been granted access to the My Account API, you will also need to leverage the <a href="https://auth0.com/docs/secure/tokens/refresh-tokens/multi-resource-refresh-token" target="_blank">Multi-Resource Refresh Token</a> feature, which enables the refresh token delivered to your application to also obtain an access token to call the My Account API. <br/><br/>
       You can quickly define a <a href="https://auth0.com/docs/secure/tokens/refresh-tokens/multi-resource-refresh-token/configure-and-implement-multi-resource-refresh-token" target="_blank">refresh token policy</a> for your application to use when requesting access tokens for the My Account API by doing the following:
       <ul>
-        <li>Navigate to <strong>Applications &gt; Applications</strong> and select your client application.</li>
+        <li>Navigate to <a href="https://manage.auth0.com/#/applications" target="_blank"><strong>Applications &gt; Applications</strong></a> and select your client application.</li>
         <li>On the <strong>Settings</strong> tab, scroll down to the <strong>Multi-Resource Refresh Token</strong> section.</li>
         <li>Select <strong>Edit Configuration</strong> and then enable the MRRT toggle for the <strong>Auth0 My Account API</strong>.</li>
       </ul>
@@ -138,7 +131,7 @@ export const Prerequisites = ({
 
   steps.push(
     <Step key="google-connection" title="Configure Google Social Integration">
-      Set up a Google developer account that allows for third-party API calls by
+      Set up a Google developer account that allows for external API calls by
       following the <a href="/integrations/google">Google Social Integration</a>{" "}
       instructions.
     </Step>
