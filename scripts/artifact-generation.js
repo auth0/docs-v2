68a74b6b9a955a51c40dee24f8e0922ba806a65e
abrien20 <75395809+abrien20@users.noreply.github.com>
2026-03-03 15:34:31 -0700

    fix: update artifact generation script (#726)

--- scripts/artifact-generation.js
+++ scripts/artifact-generation.js
@@ -109,7 +109,7 @@ function getEndpointScopes(spec) {
     const scopeList = Object.values(securityScheme);
     return [...acc, ...scopeList];
   }, []);
-  return flattenDeep(scopes);
+  return new Set(flattenDeep(scopes));
 }
 
 async function writeMdxContent(config) {
