0fe1edb6e008efdc217af944a25e5e7aac755298
Sarah West <sarah.west@okta.com>
2026-03-13 11:42:25 -0500

    feat: implement adobe tag manager

---
+++ ui/src/lib/adobe.ts
@@ -0,0 +1,24 @@
+import { config } from './config';
+
+export function initAdobe(source: string): void {
+  // load adobe tag manager script
+  const script = document.createElement('script');
+  script.src = source;
+  script.async = true;
+  script.type = 'text/javascript';
+
+  // append to body tag
+  document.head.append(script);
+}
+
+export function loadAdobeScript(): void {
+  if (typeof window === 'undefined') return;
+
+  // check if adobe tag manager is enabled
+  if (!config.adobeTagManager.enabled || !config.adobeTagManager.loaderScript) {
+    console.log('Adobe Tag Manager is disabled or script source is missing.');
+    return;
+  }
+
+  initAdobe(config.adobeTagManager.loaderScript);
+}
